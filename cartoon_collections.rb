d2f7ff884f96784d402d1d335dd76985c296cf2f
Mell Basham <mellbasham@gmail.com>
2020-11-30 23:17:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
 end
 
 def find_valid_calls(array)
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  array.find { |calls| valid_calls.include?(calls) }
-end
+    valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+    array.find { |calls| valid_calls.include?(calls) }
+  end
 end
